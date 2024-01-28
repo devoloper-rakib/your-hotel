7ab8562423b753c226e925056c7091d4ce9106ad
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-01-29 03:28:23 +0600

    layout added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,34 @@
+import {
+	BrowserRouter as Router,
+	Route,
+	Routes,
+	Navigate,
+} from 'react-router-dom';
+import Layout from './layout/Layout';
+
 function App() {
 	return (
-		<>
-			<h1 className='underline text-black '>hello frontend</h1>
-		</>
+		<Router>
+			<Routes>
+				<Route
+					path='/'
+					element={
+						<Layout>
+							<p>hello world :) </p>
+						</Layout>
+					}
+				/>
+				<Route
+					path='/search'
+					element={
+						<Layout>
+							<p>Search Page :) </p>
+						</Layout>
+					}
+				/>
+				<Route path='*' element={<Navigate to='/' />} />
+			</Routes>
+		</Router>
 	);
 }
 
