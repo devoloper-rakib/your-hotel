1a519a98cdb6cf6608a4b8161f6d637c4673921f
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-01-30 02:49:17 +0600

    auth login routes implemented

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,8 @@ import cors from 'cors';
 import 'dotenv/config';
 import mongoose from 'mongoose';
 
-import userRoutes from './routes/Users';
+import userRoutes from './routes/users';
+import authRoutes from './routes/Auth';
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 
 // Health route Checkpoint
