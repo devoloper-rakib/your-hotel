77472cac22785ec20e4de12ab3c4e7f995f3fc11
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-02-04 16:44:17 +0600

    desc, price, star input filed added

--- frontend/src/forms/ManageHotelForm/DetailsSection.tsx
+++ frontend/src/forms/ManageHotelForm/DetailsSection.tsx
@@ -22,6 +22,77 @@ const DetailsSection = () => {
 					<span className='text-red-500'> {errors.name.message} </span>
 				)}
 			</label>
+
+			<div className='flex gap-4'>
+				<label className='text-gray-700 text-sm font-bold flex-1'>
+					City
+					<input
+						type='text'
+						className='border rounded w-full py-1 px-2 font-normal'
+						{...register('city', { required: 'This filed is required' })}
+					/>
+					{errors.city && (
+						<span className='text-red-500'> {errors.city.message} </span>
+					)}
+				</label>
+
+				<label className='text-gray-700 text-sm font-bold flex-1'>
+					Country
+					<input
+						type='text'
+						className='border rounded w-full py-1 px-2 font-normal'
+						{...register('country', { required: 'This filed is required' })}
+					/>
+					{errors.country && (
+						<span className='text-red-500'> {errors.country.message} </span>
+					)}
+				</label>
+			</div>
+
+			<label className='text-gray-700 text-sm font-bold flex-1'>
+				Description
+				<textarea
+					rows={10}
+					className='border rounded w-full py-1 px-2 font-normal'
+					{...register('description', { required: 'This filed is required' })}
+				></textarea>
+				{errors.description && (
+					<span className='text-red-500'> {errors.description.message} </span>
+				)}
+			</label>
+
+			<label className='text-gray-700 text-sm font-bold max-w-[50%]'>
+				Price Per Night
+				<input
+					type='number'
+					min={1}
+					className='border rounded w-full py-1 px-2 font-normal'
+					{...register('pricePerNight', { required: 'This filed is required' })}
+				/>
+				{errors.pricePerNight && (
+					<span className='text-red-500'> {errors.pricePerNight.message} </span>
+				)}
+			</label>
+
+			<label className='text-gray-700 text-sm font-bold max-w-[50%]'>
+				Star Rating
+				<select
+					{...register('starRating', {
+						required: 'This filed is required',
+					})}
+					className='border rounded w-full p-2 text-gray-700 font-normal'
+				>
+					<option value='' className='text-sm font-bold  '>
+						Select as Rating
+					</option>
+					{[1, 2, 3, 4, 5].map((num) => (
+						<option value={num}> {num} </option>
+					))}
+				</select>
+				{errors.city && (
+					<span className='text-red-500'> {errors.city.message} </span>
+				)}
+			</label>
 		</div>
 	);
 };
