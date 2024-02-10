2c2b12cd90c2d6de90eac5557ad75b5ac522f292
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-02-10 15:25:55 +0600

    my hotel page implemented

--- frontend/src/pages/MyHotels.tsx
+++ frontend/src/pages/MyHotels.tsx
@@ -1,6 +1,23 @@
+import { useQuery } from 'react-query';
 import { Link } from 'react-router-dom';
+import { BsBuilding, BsMap } from 'react-icons/bs';
+
+import * as apiClient from '../api-client';
+import { BiHotel, BiMoney, BiStar } from 'react-icons/bi';
 
 const MyHotels = () => {
+	const { data: hotelData } = useQuery(
+		'fetchMyHotels',
+		apiClient.fetchMyHotels,
+		{
+			onError: () => {},
+		},
+	);
+
+	if (!hotelData) {
+		return <span>No Hotels Found</span>;
+	}
+
 	return (
 		<div className='space-y-5'>
 			<span className='flex justify-between'>
@@ -12,6 +29,53 @@ const MyHotels = () => {
 					Add Hotel
 				</Link>
 			</span>
+			<div className='grid grid-cols-1 gap-8'>
+				{hotelData?.map((hotel, index) => (
+					<div
+						className='flex flex-col justify-between gap-5 p-8 border rounded-lg border-slate-300'
+						key={index}
+					>
+						<h2 className='text-2xl font-bold'> {hotel.name} </h2>
+						<div className='whitespace-pre-line'>{hotel.description}</div>
+						{/* hotel types and other thing */}
+						<div className='grid grid-cols-5 gap-2'>
+							<div className='flex items-center p-3 border rounded-sm border-slate-300'>
+								<BsMap className='mr-1' />
+								{hotel.city}, {hotel.country}
+							</div>
+
+							<div className='flex items-center p-3 border rounded-sm border-slate-300'>
+								<BsBuilding className='mr-1' />
+								{hotel.type}
+							</div>
+
+							<div className='flex items-center p-3 border rounded-sm border-slate-300'>
+								<BiMoney className='mr-1' />${hotel.pricePerNight} per night
+							</div>
+
+							<div className='flex items-center p-3 border rounded-sm border-slate-300'>
+								<BiHotel className='mr-1' />
+								{hotel.adultCount} Adults, {hotel.childCount} Children
+							</div>
+
+							<div className='flex items-center p-3 border rounded-sm border-slate-300'>
+								<BiStar className='mr-1' />
+								{hotel.starRating} Star rating
+							</div>
+						</div>
+
+						{/* view details link */}
+						<span className='flex justify-end'>
+							<Link
+								className='flex p-2 text-xl font-bold text-white bg-blue-600 hover:bg-blue-500'
+								to={`/edit-hotel/${hotel._id}`}
+							>
+								View Details
+							</Link>
+						</span>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };
