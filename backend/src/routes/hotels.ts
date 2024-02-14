35f43bea5db9293a2a69f69578cabe0b3eaf0edf
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-02-14 14:06:02 +0600

    backend end point searching algorithm updated and added sorting features

--- backend/src/routes/hotels.ts
+++ backend/src/routes/hotels.ts
@@ -10,6 +10,20 @@ router.get('/search', async (req: Request, res: Response) => {
 		// point : searching
 		const query = constructSearchQuery(req.query);
 
+		let sortOptions = {};
+
+		switch (req.query.sortOption) {
+			case 'starRating':
+				sortOptions = { starRating: -1 };
+				break;
+			case 'pricePerNightAsc':
+				sortOptions = { pricePerNight: 1 };
+				break;
+			case 'pricePerNightDesc':
+				sortOptions = { pricePerNight: -1 };
+				break;
+		}
+
 		const pageSize = 5;
 		const pageNumber = parseInt(
 			req.query.page ? req.query.page.toString() : '1',
@@ -17,7 +31,10 @@ router.get('/search', async (req: Request, res: Response) => {
 
 		const skip = (pageNumber - 1) * pageSize;
 
-		const hotels = await Hotel.find(query).skip(skip).limit(pageSize);
+		const hotels = await Hotel.find(query)
+			.sort(sortOptions)
+			.skip(skip)
+			.limit(pageSize);
 
 		const total = await Hotel.countDocuments(query);
 
@@ -36,7 +53,65 @@ router.get('/search', async (req: Request, res: Response) => {
 	}
 });
 
-// POINT :  SEARCH QUERY
+// Point : searching algorithm/ query v2
+// const constructSearchQuery = (queryParams: any) => {
+// 	let constructedQuery: any = {};
+
+// 	if (queryParams.destination) {
+// 		constructedQuery.$or = [
+// 			{ city: new RegExp(queryParams.destination, 'i') },
+// 			{ country: new RegExp(queryParams.destination, 'i') },
+// 		];
+// 	}
+
+// 	if (queryParams.adultCount) {
+// 		constructedQuery.adultCount = {
+// 			$gte: parseInt(queryParams.adultCount),
+// 		};
+// 	}
+
+// 	if (queryParams.childCount) {
+// 		constructedQuery.childCount = {
+// 			$gte: parseInt(queryParams.childCount),
+// 		};
+// 	}
+
+// 	if (queryParams.facilities) {
+// 		constructedQuery.facilities = {
+// 			$all: Array.isArray(queryParams.facilities)
+// 				? queryParams.facilities.map(
+// 						(facility: string) => new RegExp(facility, 'i'),
+// 				  )
+// 				: [new RegExp(queryParams.facilities, 'i')],
+// 		};
+// 	}
+
+// 	if (queryParams.types) {
+// 		constructedQuery.type = {
+// 			$in: Array.isArray(queryParams.types)
+// 				? queryParams.types.map((type: string) => new RegExp(type, 'i'))
+// 				: [new RegExp(queryParams.types, 'i')],
+// 		};
+// 	}
+
+// 	if (queryParams.stars) {
+// 		constructedQuery.starRating = {
+// 			$in: Array.isArray(queryParams.stars)
+// 				? queryParams.stars.map((star: string) => parseInt(star))
+// 				: [parseInt(queryParams.stars)],
+// 		};
+// 	}
+
+// 	if (queryParams.maxPrice) {
+// 		constructedQuery.pricePerNight = {
+// 			$lte: parseInt(queryParams.maxPrice),
+// 		};
+// 	}
+
+// 	return constructedQuery;
+// };
+
+// POINT :  searching algorithm/ query v1
 const constructSearchQuery = (queryParams: any) => {
 	let constructedQuery: any = {};
 
